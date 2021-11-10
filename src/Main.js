d5e138a9c3ab1479acf6801ab355b3989d935012
FlamemasterNXF <flamemasternxf@gmail.com>
2021-11-09 20:20:34 -0500

    Info + Nerf

--- src/Main.js
+++ src/Main.js
@@ -6,7 +6,7 @@ function mainLoop(){
     data.time = Date.now()
     increaseUni(new Decimal(0.1).times(diff))
     for (let i=1;i<data.numbers.length;i++){
-        data.numbers[i] = data.numbers[i].plus(data.numbers[i-1].div(10).times(diff))
+        data.numbers[i] = data.numbers[i].plus(data.numbers[i-1].div(100).times(diff))
     }
     updateHTML()
 }
